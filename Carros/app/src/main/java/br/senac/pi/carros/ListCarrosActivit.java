89a59fc5a9fe9bdcef7a4906b0a881b9c3f1e8cc
Luana Rodrigues <luana3081@gmail.com>
2015-11-17 14:56:21 -0300

    mudanças

--- Carros/app/src/main/java/br/senac/pi/carros/ListCarrosActivit.java
+++ Carros/app/src/main/java/br/senac/pi/carros/ListCarrosActivit.java
@@ -1,14 +1,20 @@
 package br.senac.pi.carros;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+
+import br.senac.pi.carros.domains.Carro;
 import br.senac.pi.carros.domains.carrosDB;
 
 public class ListCarrosActivit extends AppCompatActivity {
@@ -28,6 +34,8 @@ public class ListCarrosActivit extends AppCompatActivity {
         database = carrosDB.getWritableDatabase();
         findViewById(R.id.bntListarCarros).setOnClickListener(listarCarros());
 
+        //Chamar Listener Delet
+        listView.setOnItemClickListener(deletarItem());
     }
 
     private View.OnClickListener listarCarros(){
@@ -44,5 +52,36 @@ public class ListCarrosActivit extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    //Recupera o item do banco pelo id e faz o delet
+    private AdapterView.OnItemClickListener deletarItem() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final long itemSelecionado = id;
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListCarrosActivit.this);
+                builder.setTitle("Pergunta");
+                builder.setMessage("Deseja excluir o item?");
+                builder.setPositiveButton("Editar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ListCarrosActivit.this, "Clicou em editar", Toast.LENGTH_LONG).show();
+                    }
+                });
+builder.setNegativeButton("Deletar", new DialogInterface.OnClickListener() {
+    @Override
+    public void onClick(DialogInterface dialog, int id) {
+        Log.i("carro", "ID do item selecionado" + itemSelecionado);
+        Carro carro = new Carro();
+        carro.setId(itemSelecionado);
+        carrosDB.delete(carro);
+    }
+});
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        };
+
     }
 }
